f5f4717744a64b03e142dc7e483b565c48ef8653
GyuwonHwang <113331205+kb081544@users.noreply.github.com>
2022-12-04 01:56:37 +0900

    Delete Project/Fin-term/Python File directory

--- Project/Fin-term/Python File/(1)ProcessingData.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-merged_data=pd.read_csv("merge.csv")
-#중복값 제거
-df_drop=merged_data.drop_duplicates(['Longitude', 'Latitude', 'Rubbish','Plastics','Cans','Glass','Papers'])
-#필요없는 정보 제거. 쓰레기 유무와 위도 경도만 필요하므로 나머지 열은 drop 시킨다.
-df=df_drop.drop(['Number', 'Name', 'Widths', 'Hights'], axis=1)
-df.to_csv("Data.csv")
